ebb63fa3e202b8b472f4a08e30723a7a169295a8
soorya <sooryatms@gmail.com>
2020-07-21 01:22:29 -0700

    integrated motion detection and face detection

--- motion_still_detection.py
+++ motion_still_detection.py
@@ -1,13 +1,22 @@
 import cv2
 import sys
 import threading
+import numpy as np
+from face_detector import  face_detecter
 # capturing first frame without any motion
 baseline_image=None
 status_list=[None,None]
 video=cv2.VideoCapture(0)
-x = 10 #position of text
-y = 20 #position of text
+x1 = 10 #position of text
+y1 = 20 #position of text
+def mse(imageA, imageB):
 
+	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+	err /= float(imageA.shape[0] * imageA.shape[1])
+	
+	# return the MSE, the lower the error, the more "similar"
+	# the two images are
+	return err
 
 while True:
     check, frame = video.read()
@@ -22,17 +31,20 @@ while True:
     threshold=cv2.threshold(delta, 30, 255, cv2.THRESH_BINARY)[1]
     (contours,_)=cv2.findContours(threshold,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     font = cv2.FONT_HERSHEY_SIMPLEX
+    if not contours:
+        cv2.putText(frame,'no motion detected!',(0,50), font, 1, (200,255,155), 2, cv2.LINE_AA)
     for contour in contours:
-        cv2.putText(frame,'no motion detected!',(0,130), font, 1, (200,255,155), 2, cv2.LINE_AA)
-        #cv2.PutText(frame,"status: NO MOTION DETECTED", (x,y),cv2.FONT_HERSHEY_SIMPLEX, 255) #Draw the text
         if cv2.contourArea(contour) < 10000:
             continue
-        (x, y, w, h)=cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 1)
-        cv2.putText(frame,'motion detected!',(0,130), font, 1, (200,255,155), 2, cv2.LINE_AA) 
-        print('motion detected')
-
-
+        else:
+            (x, y, w, h)=cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 1)
+            cv2.putText(frame,'motion detected!',(0,20), font, 1, (200,255,155), 2, cv2.LINE_AA)
+            faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+            faces = faceCascade.detectMultiScale(gray_frame,scaleFactor=1.3, minNeighbors=3,minSize=(30, 30))
+            print("[INFO] Found {0} Faces!".format(len(faces)))
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.imshow("gray_frame Frame",gray_frame)
     cv2.imshow("Delta Frame",delta)
     cv2.imshow("Threshold Frame",threshold)
